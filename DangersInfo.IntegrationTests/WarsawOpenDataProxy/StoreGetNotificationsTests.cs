8c5281fef5604f9cf77d53f224b8ee622e7f4190
Robert Wąsik <Robert.Wasik@materialise.com>
2019-06-03 10:32:00 +0200

    Add integration and unit tests

---
+++ DangersInfo.IntegrationTests/WarsawOpenDataProxy/StoreGetNotificationsTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DangersInfo.Configuration.Configs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WarsawOpenData.Proxy.Services;
+using WarsawOpenData.Proxy.Utils;
+
+namespace DangersInfo.IntegrationTests.WarsawOpenDataProxy
+{
+    [TestClass]
+    public class StoreGetNotificationsTests
+    {
+        private IOptions<WarsawOpenDataSettings> _options;
+
+        [TestInitialize]
+        public void Init()
+        {
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false);
+
+            IConfigurationRoot configuration = configurationBuilder.Build();
+
+            var settings = configuration.GetSection("WarsawOpenDataSettings").Get<WarsawOpenDataSettings>();
+            _options = Options.Create(settings);            
+        }
+
+        [TestMethod]
+        public async Task GetSerialObjectsByContainerIdAsync_ReturnsContainerWithSerialObjects()
+        {
+            // arrange
+            var service = new WarsawOpenDataService(new RemoteClient(), _options);
+
+            // act
+            var notifications = await service.GetReportedNotificationsAsync();
+
+            // assert
+            Assert.IsNotNull(notifications);
+            Assert.IsTrue(notifications.Any());
+        }
+    }
+}
---
+++ DangersInfo.Tests/WarsawOpenDataProxy/Services/WarsawOpenDataServiceTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DangersInfo.Configuration.Configs;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using WarsawOpenData.Proxy.Contracts;
+using WarsawOpenData.Proxy.Services;
+using WarsawOpenData.Proxy.Utils;
+
+namespace DangersInfo.Tests.WarsawOpenDataProxy.Services
+{
+    [TestClass]
+    public class WarsawOpenDataServiceTests
+    {
+        private Mock<IRemoteClient> _remoteClientMock;
+        private Mock<IOptions<WarsawOpenDataSettings>> _settingsMock;
+
+        private IWarsawOpenDataService _warsawOpenDataService;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _remoteClientMock = new Mock<IRemoteClient>();
+            _settingsMock = new Mock<IOptions<WarsawOpenDataSettings>>();
+            _settingsMock.Setup(options => options.Value).Returns(new WarsawOpenDataSettings { ApiId = "id", ApiKey = "key", ApiUrl = "url" });
+
+            _warsawOpenDataService = new WarsawOpenDataService(_remoteClientMock.Object, _settingsMock.Object);
+        }
+
+        [TestMethod]
+        public async Task GetReportedNotificationsAsync_WhenResponseSucceedeed_ReturnsNotifications()
+        {
+            // arrange
+            _remoteClientMock.Setup(client => client.GetAsync<NotificationsResponse>(It.IsAny<string>())).ReturnsAsync(
+                new NotificationsResponse
+                {
+                    Result = new NotificationsResult
+                    {
+                        Success = true,
+                        Data = new NotificationsData {Notifications = new List<Notification> {new Notification()}}
+                    }
+                });
+            
+            // act
+            var notifications = await _warsawOpenDataService.GetReportedNotificationsAsync();
+
+            // assert
+            Assert.AreEqual(1, notifications.Count());
+        }
+
+        [TestMethod]
+        public async Task GetReportedNotificationsAsync_WhenResponseFailed_ThrowsAnException()
+        {
+            // arrange
+            _remoteClientMock.Setup(client => client.GetAsync<NotificationsResponse>(It.IsAny<string>())).ReturnsAsync(
+                new NotificationsResponse
+                {
+                    Result = new NotificationsResult
+                    {
+                        Success = false,
+                        ResponseCode = "999",
+                        ResponseDesc = "failure"
+                    }
+                });
+
+            // act
+            try
+            {
+                await _warsawOpenDataService.GetReportedNotificationsAsync();
+            }
+            catch(Exception ex)
+            {
+                // assert
+                Assert.AreEqual("[WarsawOpenDataService] 19115store_getNotifications failed. Response code: 999. Description: failure", ex.Message);
+            }
+        }
+    }
+}
